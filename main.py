28f596277ae819c53764232957e2328472b23d8a
ValeriyMakarov <misteranon.com@gmail.com>
2023-03-13 22:43:40 +0200

    types

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def print_type(*value):
+    if len(value)==1 and type(value[0]) in [list, dict, set]:
+        print(type(value[0]))
+    else:
+        print(type(value))
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_type(1,2,3)
+    print_type([1,2,3])
+    print_type({1,2,3})
+    print_type({1:1,2:2,3:3})
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
