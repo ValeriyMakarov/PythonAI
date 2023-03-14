7fe5c03cf695ed4ef947a9c8ddf2b4f22f79a8df
ValeriyMakarov <misteranon.com@gmail.com>
2023-03-14 23:10:23 +0200

    added NeuralNetwork class

---
+++ NetWork.py
@@ -0,0 +1,12 @@
+class NeuralNetwork:
+    def __init__(self, input_layer_size:int, hidden_layer_size:int, output_layer_size:int, learning_rate):
+        self.input_layer_size = input_layer_size
+        self.hidden_layer_size = hidden_layer_size
+        self.output_layer_size = output_layer_size
+        self.learning_rate = learning_rate
+
+    def train(self):
+        pass
+
+    def query(self):
+        pass
--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-
-def print_type(*value):
-    if len(value)==1 and type(value[0]) in [list, dict, set]:
-        print(type(value[0]))
-    else:
-        print(type(value))
+from NetWork import NeuralNetwork
 
 if __name__ == '__main__':
-    print_type(1,2,3)
-    print_type([1,2,3])
-    print_type({1,2,3})
-    print_type({1:1,2:2,3:3})
-
+    NeuralNetwork(20,20,2,0.3)
